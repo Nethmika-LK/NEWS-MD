620459913e14f910206b84c1d5550c67575a2148
Nethmika-LK <nethumd65@gmail.com>
2025-03-04 20:36:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '2NEQBJgR#wAhZrO3Foaybk8_dqHAUQmJHcPWpUTCcTNNpY4HP-hE', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'BmNjGDwZ#uSm8wn1f-lFvbvczkBoE8kW8RwisgzTibU-kBSRiORo', // Enter Your Session ID
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',    // Enter Your MongoDB URL
 };
