e4aa1c29b9d34caefb7699daf1b005126b96d6d6
Nethmika-LK <nethumd65@gmail.com>
2025-03-19 11:33:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,6 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'BmNjGDwZ#uSm8wn1f-lFvbvczkBoE8kW8RwisgzTibU-kBSRiORo', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',    // Enter Your MongoDB URL
+    SESSION_ID: process.env.SESSION_ID || 'XoxwlaIa#KpE-K87gN5ZX9Qwn3tN0tSlW6HyBJ03Yeb30gwkDkeU', 
 };
